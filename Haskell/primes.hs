191432b4c97347be68619d8ca42a65a2919dc81f
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2025-02-07 16:03:37 +0100

    typos corrected

--- Haskell/primes.hs
+++ Haskell/primes.hs
@@ -1,7 +1,7 @@
---primes :: [Integer]
+primes :: [Integer]
 primes = sieve [2..]
 
---sieve :: [Integer] -> [Integer]
+sieve :: [Integer] -> [Integer]
 sieve (p:ns) = p : sieve [n | n <- ns, mod n p /= 0]
 
 -- compute the ten thousandth prime number
